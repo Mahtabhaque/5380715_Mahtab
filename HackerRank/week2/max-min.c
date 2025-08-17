827134b630ad3c3cef2f22ec96a1117d27e19593
Mahtabhaque <mahtabhaque0786@gmail.com>
2025-08-17 21:31:40 +0530

    max-min

---
+++ HackerRank/week2/max-min.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int cmp(const void *a, const void *b) {
+    return (*(int*)a - *(int*)b);
+}
+
+int main() {
+    int n, k;
+    scanf("%d", &n);
+    scanf("%d", &k);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+
+    qsort(arr, n, sizeof(int), cmp);
+
+    int minUnfair = arr[k-1] - arr[0];
+    for (int i = 1; i <= n - k; i++) {
+        int diff = arr[i + k - 1] - arr[i];
+        if (diff < minUnfair) minUnfair = diff;
+    }
+
+    printf("%d\n", minUnfair);
+    return 0;
+}
